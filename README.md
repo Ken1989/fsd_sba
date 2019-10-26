dc2c4284ffbe9d66b73361b8a2e5ad9baa97c1fe
Ken <dingkebuaa@163.com>
2019-10-26 16:05:01 +0800

    20191026-dingke-init config server source code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # fsd_sba
 
 1. Create the repository and readme file
+2. Create the eureka server source code 
+3. Create the config server instance and create the yml files for User_service, Admin_service and Mentor_service in config repository
+   https://github.com/Ken1989/ConfigData
+4. 
+ 
---
+++ sba_config/src/main/java/cn/ibm/com/SbaConfigApplication.java
@@ -0,0 +1,15 @@
+package cn.ibm.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SbaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaConfigApplication.class, args);
+	}
+
+}
